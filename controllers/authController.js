8386cc385a75a3273742bfa956172c13b8899ec4
Ajay_Bains <ajay.bains@codvo.ai>
2023-04-02 11:42:34 +0530

    login route added

--- controllers/authController.js
+++ controllers/authController.js
@@ -29,12 +29,34 @@ const register = async (req, res) => {
   //     httpOnly: true,
   //     expires: new Date(Date.now() + oneDay),
   //   });
-  attachCookiesToResponse({res,user:tokenUser})
+  attachCookiesToResponse({ res, user: tokenUser });
   res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
 const login = async (req, res) => {
-  res.send("login user");
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new CustomError.BadRequestError("Please provide email and password");
+  }
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new CustomError.UnauthenticatedError(
+      "Invalid credentials or user doesnt exists"
+    );
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+
+  if (!isPasswordCorrect) {
+    throw new CustomError.UnauthenticatedError(
+      "Invalid credentials or incorrect password"
+    );
+  }
+  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  attachCookiesToResponse({ res, user: tokenUser });
+  res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
+
 const logout = async (req, res) => {
   res.send("logout user");
 };
