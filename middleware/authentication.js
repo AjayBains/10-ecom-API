35edf5ac17ad87d8a4b003f9236d2a0c8ffee01d
Ajay_Bains <ajay.bains@codvo.ai>
2023-04-12 03:27:40 +0530

    admin authorization to view all users added

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -5,13 +5,32 @@ const authenticateUser = async (req, res, next) => {
   const token = req.signedCookies.token;
 
   if (!token) {
-    console.log("error,no totken present");
-  } else {
-    console.log("token present");
+    throw new CustomErr.UnauthenticatedError("AUtnetication invalid");
   }
-  next();
+  try {
+    const payload = isTokenValid({ token });
+    console.log("Payloard", payload);
+    req.user = {
+      name: payload?.name,
+      userId: payload?.userId,
+      role: payload?.role,
+    };
+    next();
+  } catch (error) {
+    throw new CustomErr.UnauthenticatedError("AUtnetication invalid");
+  }
 };
 
+const authorizePermissions = (...roles) => {
+ return (req,res,next)=>{
+
+   if (!roles.includes(req.user.role)) {
+     throw new CustomErr.UnauthorizedError("Unauthorized to access this route");
+   }
+   next();
+ }
+};
 module.exports = {
   authenticateUser,
+  authorizePermissions,
 };
