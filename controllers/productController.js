a745bc6d47e3226a82a9f68ee5d8eefcae2dfdf6
Ajay_Bains <ajay.bains@codvo.ai>
2023-04-20 11:57:05 +0530

    product crud and upload image controller functionality added

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/Product");
 const { StatusCodes } = require("http-status-codes");
 const customError = require("../errors");
+const path = require("path");
 
 const createProduct = async (req, res) => {
   req.body.user = req.user.userId;
@@ -9,19 +10,65 @@ const createProduct = async (req, res) => {
   res.send("createProduct");
 };
 const getAllProducts = async (req, res) => {
-  res.send("getAllProducts");
+  const products = await Product.find({});
+  res.status(StatusCodes.OK).json({ products, count: products.length });
 };
 const getSingleProduct = async (req, res) => {
-  res.send("getSingleProduct");
+  const { id: productId } = req.params;
+  const product = await Product.find({ _id: productId });
+  if (!product) {
+    throw new customError.NotFoundError(`No product with id :${productId}`);
+  }
+  res.status(StatusCodes.OK).json({ product });
 };
 const updateProduct = async (req, res) => {
-  res.send("updateProduct");
+  const { id: productId } = req.params;
+  const product = await Product.findOneAndUpdate({ _id: productId }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!product) {
+    throw new customError.NotFoundError(`No product with id :${productId}`);
+  }
+  res.status(StatusCodes.OK).json({ product });
 };
 const deleteProduct = async (req, res) => {
-  res.send("deleteProduct");
+  const { id: productId } = req.params;
+  const product = await Product.findOne({ _id: productId });
+
+  if (!product) {
+    throw new customError.NotFoundError(`No product with id :${productId}`);
+  }
+
+  await product.remove();
+
+  res.status(StatusCodes.OK).json({ msg: "Success product removed" });
 };
 const uploadImage = async (req, res) => {
-  res.send("uploadImage");
+  // console.log('log re files',req.files);
+  if (!req.files) {
+    throw new customError.BadRequestError("No File Uploaded");
+  }
+  const productImage = req.files.image;
+
+  if (!productImage.mimetype.startsWith("image")) {
+    throw new customError.BadRequestError("Please Upload Image");
+  }
+  const maxSize = 1024 * 1024;
+
+  if (productImage.size > maxSize) {
+    throw new customError.BadRequestError(
+      "Please upload image smaler than 1MB"
+    );
+  }
+
+  const imagePath = path.join(
+    __dirname,
+    "../public/uploads/" + `${productImage.name}`
+  );
+
+  await productImage.mv(imagePath);
+  res.status(StatusCodes.OK).json({ image: `/uploads/${productImage.name}` });
 };
 
 module.exports = {
